435c5930f4cf148e70ab51ff74e8f56eed73dffe
Dan <dpereda@gmail.com>
2017-01-20 11:37:19 -0500

    Pulling in correct data and displaing it from wiki   Title and snippet. Need to get search button working Need to get linking from results out working Need style

--- main.js
+++ main.js
@@ -5,10 +5,17 @@ $("#searchWiki").click(function(){
     $.getJSON("https://en.wikipedia.org/w/api.php?action=query&format=json&origin=*&list=search&srsearch=racing&srwhat=text", function( data ){
        var items = [];
        console.log(data);
-      $.each( data, function(key, val) {
-        items.push("<p>"+val+"</p>")
+       var i =0;
+
+
+      $.each(data.query.search, function(key, val) {
+          items.push(val);
+          //alert(data.query.search[i].title);
+          $("#wikiIT").append("<li>"+data.query.search[i].title+" "+ data.query.search[i].snippet+"</li>");
+          i++
          });
          console.log(items);
+         console.log(data.query.search);
         //$('#rQuote').html(items);
         //theQuote = $("p:first").text()
       });//end of get json
